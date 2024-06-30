e85b5e3ee10247c236caa89e852c96a73defc160
Ravi-dhanani <117377500+Ravi-dhanani@users.noreply.github.com>
2024-06-30 15:36:26 +0530

    react quell color and bg color add

--- src/component/common/RichTextEditer.tsx
+++ src/component/common/RichTextEditer.tsx
@@ -2,6 +2,71 @@ import ReactQuill from "react-quill";
 interface IRichTextEditerProps {
   field: any;
 }
+const modules = {
+  toolbar: [
+    [{ header: "1" }, { header: "2" }, { font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      {
+        color: [
+          "red",
+          "orange",
+          "yellow",
+          "green",
+          "blue",
+          "indigo",
+          "violet",
+          "black",
+          "white",
+          "gray",
+          "brown",
+          "pink",
+          "purple",
+          "cyan",
+          "magenta",
+          "olive",
+          "lime",
+          "teal",
+          "navy",
+          "maroon",
+          "silver",
+          "gold",
+          "transparent",
+        ],
+      },
+      { background: [] },
+    ],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["clean"],
+  ],
+  clipboard: {
+    // toggle to add extra line breaks when pasting HTML:
+    matchVisual: false,
+  },
+};
+
+const formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "color",
+  "background",
+];
+
 export default function RichTextEditer(props: IRichTextEditerProps) {
   return (
     <div>
@@ -9,7 +74,12 @@ export default function RichTextEditer(props: IRichTextEditerProps) {
         theme="snow"
         value={props.field.value}
         onChange={(content: any) => props.field.onChange(content)}
-        style={{ height: "400px" }}
+        style={{
+          height: "400px",
+          backgroundColor: "#D3D3D3",
+        }}
+        formats={formats}
+        modules={modules}
       />
     </div>
   );
